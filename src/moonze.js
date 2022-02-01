866cac00ce6aec347b9af28d1b2a6fa75b8ce7b9
lnuvy <hangeulgod@naver.com>
2022-02-01 15:55:42 +0900

    11021

--- src/moonze.js
+++ src/moonze.js
@@ -5,15 +5,23 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-let answer = "";
+let testCase = 100;
+let valueArr = [];
 
 rl.on("line", (line) => {
-  const input = line;
-  for (let i = input; i >= 1; i--) {
-    answer += i;
-    answer += "\n";
+  if (testCase === valueArr.length) {
   }
+  const input = line.split(" ");
+  if (input.length === 1) testCase = input[0];
+  else {
+    const A = Number(input[0]);
+    const B = Number(input[1]);
+    valueArr.push(A + B);
+  }
+  console.log(input, valueArr.length, testCase);
 }).on("close", () => {
-  console.log(answer);
+  for (let i = 0; i < testCase; i++) {
+    console.log(`Case #${i + 1}: ${valueArr[i]}`);
+  }
   process.exit();
 });
