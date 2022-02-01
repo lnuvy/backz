75118040856d55825d4b1b384d25cacbf74a2dab
lnuvy <hangeulgod@naver.com>
2022-02-01 15:58:53 +0900

    zz

--- src/moonze.js
+++ src/moonze.js
@@ -9,19 +9,20 @@ let testCase = 100;
 let valueArr = [];
 
 rl.on("line", (line) => {
-  if (testCase === valueArr.length) {
-  }
   const input = line.split(" ");
   if (input.length === 1) testCase = input[0];
   else {
     const A = Number(input[0]);
     const B = Number(input[1]);
-    valueArr.push(A + B);
+    valueArr.push([A, B]);
   }
-  console.log(input, valueArr.length, testCase);
 }).on("close", () => {
   for (let i = 0; i < testCase; i++) {
-    console.log(`Case #${i + 1}: ${valueArr[i]}`);
+    console.log(
+      `Case #${i + 1}: ${valueArr[i][0]} + ${valueArr[i][1]} = ${
+        valueArr[i][0] + valueArr[i][1]
+      }`
+    );
   }
   process.exit();
 });
