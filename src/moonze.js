1fba3424b25b63329e8e01db1858a215be054b4a
lnuvy <hangeulgod@naver.com>
2022-02-01 15:27:54 +0900

    2741번

--- src/moonze.js
+++ src/moonze.js
@@ -6,20 +6,12 @@ const rl = readline.createInterface({
 });
 
 let answer = "";
-let testCase = 0;
 
 rl.on("line", (line) => {
-  const input = line.split(" ");
-  if (testCase === answer.split("\n").length) {
-    rl.close();
-  }
-
-  if (input.length === 2) {
-    const A = Number(input[0]);
-    const B = Number(input[1]);
-    answer += A + B + "\n";
-  } else {
-    testCase = Number(input[0]);
+  const input = line;
+  for (let i = 1; i <= input; i++) {
+    answer += i;
+    answer += "\n";
   }
 }).on("close", () => {
   console.log(answer);
